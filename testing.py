ee3236260b9955e809ee7b79d527117148d5488f
Àlex Martín <alex.martin03@estudiant.upf.edu>
2023-03-29 20:17:59 +0200

    first legato approach

--- testing.py
+++ testing.py
@@ -1,21 +1,22 @@
 from music21 import *
+import copy
 
-s = converter.parse("etudes/xml/Legato/Kayser35.mxl")
-new_voice = stream.Voice(id=2)
-ts = s.recurse().getElementsByClass(meter.TimeSignature)[0]
-new_voice.append(meter.TimeSignature(f"{ts.numerator}/{ts.denominator}"))
+s = converter.parse("etudes/xml/Legato/Kayser34.mxl")
+s_legato = copy.deepcopy(s)
 slur_bounds = []
 
-for xml_slur in s.flat.getElementsByClass('Slur'):
+for xml_slur in s_legato.flat.getElementsByClass('Slur'):
     start = xml_slur.getFirst()
     end = xml_slur.getLast()
     slur_bounds.append([start, end])
 
-
 dummy = False
-for xml_note in s.flat.notesAndRests:
+for xml_note in s_legato.flat.notesAndRests:
 
     print("Selecting note ", xml_note)
+    xml_rest = note.Rest()
+    xml_rest.duration.quarterLength = xml_note.duration.quarterLength
+    xml_rest.offset = xml_note.offset
 
     if slur_bounds:
         if xml_note == slur_bounds[0][0] and not slur_bounds[0][0].articulations:
@@ -25,14 +26,13 @@ for xml_note in s.flat.notesAndRests:
         dummy = False
 
     if dummy:
-        print(f"Appending note {xml_note}")
-        new_voice.append(xml_note)
+        pass
 
     elif not dummy:
-        xml_rest = note.Rest()
-        xml_rest.duration.quarterLength = xml_note.duration.quarterLength
-        xml_rest.offset = xml_note.offset
-        new_voice.append(xml_rest)
+        measure = xml_note.getContextByClass('Measure')
+        #measure.remove(xml_note)
+        measure.replace(xml_note, xml_rest)
+        print(f"Replacing {xml_note} by rest")
 
     if slur_bounds:
         if xml_note == slur_bounds[0][1]:
@@ -43,8 +43,8 @@ for xml_note in s.flat.notesAndRests:
     else:
         dummy = False
 
-s.append(new_voice)
-s.write("xml", "legato_output.xml")
+
+s_legato.write("xml", "legato_silence_output.xml")
 
 
 # s.write("midi", "legato_test.midi")
