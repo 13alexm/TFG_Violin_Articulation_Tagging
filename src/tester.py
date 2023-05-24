08ef6944d109e5639e79ada793646c62d4a5e994
Àlex Martín <alex.martin03@estudiant.upf.edu>
2023-05-24 22:18:32 +0200

    timestamp csv

--- src/tester.py
+++ src/tester.py
@@ -1,14 +1,14 @@
 from music21 import *
 from staccato import get_staccato
+from legato import get_legato
+from detache import get_detache
+import generate_midi as gm
 
-file = converter.parse('etudes/telemann.mxl')
+file = converter.parse('etudes/xml/Staccato/wohlfahrt24_2.mxl')
 
 
-get_staccato(file, 't_stac')
-
-"""
-get_legato(file, 't_leg')
-get_detache(file, 't_det')
-merge4midi('t_stac.midi', 't_leg.midi', 't_det.midi', 't_stac_full.midi', 't_final')
-"""
+get_staccato(file, 'w242_stac')
+get_legato(file, 'w242_leg')
+get_detache(file, 'w242_det')
+gm.merge4midi('w242_stac.midi', 'w242_leg.midi', 'w242_det.midi', 'w242_stac_full.midi', 'w242_final')
 
--- src/timestamp.py
+++ src/timestamp.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pretty_midi
-import pandas
+import librosa
 
 
 def timestamp(midi_file):
@@ -32,48 +32,50 @@ def timestamp(midi_file):
     start = None
     end = None
     articulation = [None, None]
-
     stamps = []
     idx = 0
+
     for time, current_articulation in notes_starts_sorted:
-        if len(current_articulation) > 0:
-            if time is None:
-                start = time
-                end = notes_ends_sorted[idx][0]
-                articulation = current_articulation
-            elif current_articulation != articulation:
-                print(f'Time segment starts in {start}, ends in {end} with articulation {articulation[0]}')
-                stamps += [[start, end, articulation[0]]]
-                start = time
-                end = notes_ends_sorted[idx][0]
-                articulation = current_articulation
-            else:
-                end = notes_ends_sorted[idx][0]
+        if time is None:
+            start = time
+            end = time
+            articulation = current_articulation
+        elif current_articulation != articulation:
+            print(f'Time segment starts in {start}, ends in {end} with articulation {articulation[0]}')
+            stamps += [[start, end, articulation[0]]]
+            start = time
+            end = time
+            articulation = current_articulation
+        else:
+            end = time
         idx += 1
+
     if articulation is not None:
         print(f'Time segment starts in {start}, ends in {notes_ends_sorted[-1][0]} with articulation {articulation[0]}')
         stamps.append([start, notes_ends_sorted[-1][0], articulation[0]])
 
     for idx, value in enumerate(stamps):
+        match value[2]:
+            case 6:
+                value[2] = 'Staccato'
+            case 48:
+                value[2] = 'Legato'
+            case 71:
+                value[2] = 'Detache'
+
+    for idx, value in enumerate(stamps):
         if value[2] == 40:
             stamps.pop(idx)
-        elif value[2] == 6:
-            value[2] = 'Staccato'
-        elif value[2] == 48:
-            value[2] = 'Legato'
-        elif value[2] == 71:
-            value[2] = 'Detache'
+
     stamps.pop(0)
     return stamps
 
 
 def write_time_stamps(ts, out_name):
-    transposed = list(map(list, zip(*ts)))
     df = pd.DataFrame(ts)
-    df.to_csv(out_name+'.csv', index=False, header=False)
+    df.to_csv(out_name + '.csv', index=False, header=False)
 
 
 if __name__ == '__main__':
-    time_stamps = timestamp('w24_final.midi')
-    write_time_stamps(time_stamps, "timeeeeeeeeees")
-    midi_data = pretty_midi.PrettyMIDI('w24_final.midi')
+    time_stamps = timestamp('wohlfahrt.midi')
+    write_time_stamps(time_stamps, "w24_timestamps")
