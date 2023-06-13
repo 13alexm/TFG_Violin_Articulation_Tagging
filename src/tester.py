99b2db8989fe8a25255bcfa93d95626d382959b3
Àlex Martín <alex.martin03@estudiant.upf.edu>
2023-06-13 02:22:00 +0200

    final dataset

--- src/tester.py
+++ src/tester.py
@@ -1,12 +1,59 @@
+import os
+import timestamp as ts
 
-import generate_midi as gm
 
-file = 'etudes/xml/Staccato/kreutzer4.mxl'
+def change_name(n):
+    parts = n.split("_")
+    idx = parts[-1]
+    nn = parts[0]
+    print(nn + '_' + idx)
+    return nn + '_' + idx
 
-gm.get_midi_with_articulations(file)
 
-#get_staccato(file, 'k4_stac')
-#get_legato(file, 'k4_leg')
-#get_detache(file, 'k4_det')
-#gm.merge4midi('k4_stac.midi', 'k4_leg.midi', 'k4_det.midi', 'k4_stac_full.midi', 'k4_final')
+def middle(n):
+    parts = n.split("_")
+    print(n)
+    nn = parts[2]
+    return nn
 
+
+def rename(n, ren):
+    parts = n.split("_")
+    parts[2] = ren
+    return "_".join(parts)
+
+
+# os.makedirs('Alignments/Staccato/Timestamps', exist_ok=True)
+# for filename in os.listdir("Alignments/Staccato"):
+#     if os.path.isfile(os.path.join("Alignments/Staccato", filename)):
+#         fullpath = os.path.join("Alignments/Staccato", filename)
+#         name = os.path.splitext(filename)[0]
+#
+#         ts.write_time_stamps(fullpath, 'Alignments/Staccato/Timestamps/' + change_name(name).lower())
+
+# os.makedirs('Alignments/Legato/Timestamps', exist_ok=True)
+# for filename in os.listdir("Alignments/Legato"):
+#     if os.path.isfile(os.path.join("Alignments/Legato", filename)):
+#         fullpath = os.path.join("Alignments/Legato", filename)
+#         name = os.path.splitext(filename)[0]
+#
+#         ts.write_time_stamps(fullpath, 'Alignments/Legato/Timestamps/' + change_name(name).lower())
+
+for filename in os.listdir("Data_Segments"):
+    name = os.path.splitext(filename)[0]
+    if middle(name) == 40:
+        try:
+            os.remove(filename)
+        except:
+            print("Error")
+    if middle(name) == "6":
+        try:
+            os.rename(os.path.join("Data_Segments", filename), rename(os.path.join("Data_Segments", filename), "Staccato"))
+        except:
+            print("Cannot perform operation for", filename)
+
+    # if middle(name) == "6":
+    #     try:
+    #         os.rename(filename, rename(filename, "Staccato"))
+    #     except:
+    #         print("Cannot perform operation for", filename)
--- src/timestamp.py
+++ src/timestamp.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pretty_midi
 import os
 from pydub import AudioSegment
+import glob
 
 
 def timestamp(midi_file):
@@ -40,7 +41,7 @@ def timestamp(midi_file):
             end = time
             articulation = current_articulation
         elif current_articulation != articulation:
-            #print(f'Time segment starts in {start}, ends in {end} with articulation {articulation[0]}')
+            # print(f'Time segment starts in {start}, ends in {end} with articulation {articulation[0]}')
             stamps += [[start, end, articulation[0]]]
             start = time
             end = time
@@ -50,23 +51,29 @@ def timestamp(midi_file):
         idx += 1
 
     if articulation is not None:
-        #print(f'Time segment starts in {start}, ends in {notes_ends_sorted[-1][0]} with articulation {articulation[0]}')
+        # print(f'Time segment starts in {start}, ends in {notes_ends_sorted[-1][0]} with articulation {articulation[0]}')
         stamps.append([start, notes_ends_sorted[-1][0], articulation[0]])
 
     for idx, value in enumerate(stamps):
         match value[2]:
             case 6:
                 value[2] = 'Staccato'
+            case [6]:
+                value[2] = 'Staccato'
             case 48:
                 value[2] = 'Legato'
-            case 71:
-                value[2] = 'Detache'
+            case [48]:
+                value[2] = 'Legato'
+            case other:
+                stamps.pop(idx)
 
-    for idx, value in enumerate(stamps):
-        if value[2] == 40:
-            stamps.pop(idx)
+    # for idx, value in enumerate(stamps):
+    #     if value[2] == 40:
+    #         stamps.pop(idx)
 
     stamps.pop(0)
+    if not stamps:
+        stamps.append([notes_starts_sorted[0][0], notes_ends_sorted[-1][0], notes_ends_sorted[-1][1][0]])
     return stamps
 
 
@@ -80,18 +87,59 @@ def cut_audio(audio_file, timestamps, output_dir='Data_Segments'):
     df = pd.read_csv(timestamps)
 
     os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(output_dir+'/Staccato', exist_ok=True)
+    os.makedirs(output_dir+'/Legato', exist_ok=True)
 
     for index, row in df.iterrows():
         start = row[0] * 1000  # Converting time from seconds to milliseconds
         end = row[1] * 1000
+        # if row[2][0] == '6' or row[2][0] == 'S':
+        #     row[2] = 'Staccato'
+        # elif row[2][0] == '48' or row[2][0] == 48 or row[2][0] == 'L':
+        #     row[2] = 'Legato'
+        # elif row[2][0] == '40':
+        #     break
         label = row[2]
+        # if (end - start) > 1000:
+        #     break
 
-        output_file = os.path.join(output_dir, f"{label}_segment_{index}.wav")
+        name = os.path.basename(audio_file)
+        name = os.path.splitext(name)[0]
 
         audio = AudioSegment.from_file(audio_file)
         sliced_audio = audio[start:end]
+        # if label == 'Staccato':
+        #     output_file = os.path.join(output_dir+'/Staccato', f"{name}_{label}_segment_{index}.wav")
+        #     sliced_audio.export(output_file, format='wav')
+        # if label == 'Legato':
+        #     output_file = os.path.join(output_dir + '/Legato', f"{name}_{label}_segment_{index}.wav")
+        #     sliced_audio.export(output_file, format='wav')
+        # else:
+        output_file = os.path.join(output_dir, f"{name}_{label}_segment_{index}.wav")
         sliced_audio.export(output_file, format='wav')
 
 
 if __name__ == '__main__':
-    cut_audio('Alignments/w24.wav', 'Alignments/w24_timestamps.csv')
+    output_dir = 'Data_Segments'
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(output_dir+'/Staccato', exist_ok=True)
+    os.makedirs(output_dir+'/Legato', exist_ok=True)
+    audio_directory = 'Recordings/Staccato'
+    csv_directory = 'Alignments/Staccato/Timestamps'
+
+    for audio_filee in os.listdir("Recordings/Staccato"):
+        for csv_file in os.listdir("Alignments/Staccato/Timestamps"):
+            audio_filename = os.path.splitext(os.path.basename(audio_filee))[0]
+            csv_filename = os.path.splitext(os.path.basename(csv_file))[0]
+            if audio_filename == csv_filename:
+                print(audio_filename, csv_filename)
+                cut_audio('Recordings/Staccato/' + audio_filee, 'Alignments/Staccato/Timestamps/' + csv_file)
+
+    for audio_filee in os.listdir("Recordings/Legato"):
+        for csv_file in os.listdir("Alignments/Legato/Timestamps"):
+            audio_filename = os.path.splitext(os.path.basename(audio_filee))[0]
+            csv_filename = os.path.splitext(os.path.basename(csv_file))[0]
+            if audio_filename == csv_filename:
+                print(audio_filename, csv_filename)
+                cut_audio('Recordings/Legato/' + audio_filee, 'Alignments/Legato/Timestamps/' + csv_file)
+
